ef953511ad6b5f701aa5b89af8e44f3baca995c2
Jalonss <alonss739@gmail.com>
2022-10-02 14:01:01 -0600

    Config format

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,16 +1,6 @@
 module.exports = {
-    printWidth: 100,
-    tabWidth: 2,
+    semi: false,
     singleQuote: true,
-    semi: true,
-    trailingComma: 'all',
-    arrowParens: 'always',
-    overrides: [
-        {
-            files: '*.{js,jsx,tsx,ts,scss,json,html}',
-            options: {
-                tabWidth: 4,
-            },
-        },
-    ],
-};
+    tabWidth: 4,
+    trailingComma: 'none'
+}
